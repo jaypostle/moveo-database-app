8357856dc031041628a23fa41a942d0b349a140f
Jayson Postle <jaysondigitalmcc@gmail.com>
2022-05-03 15:33:33 -0700

    production crew, trailer

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -33,7 +33,6 @@ function SingleMovie( { movie }) {
           alt={movie.title}/>}
       </div>
       <div className="single-movie-info">
-        {/* <img src={noPoster} alt='no poster available' /> */}
         <article>
             <h2>{movie.title}</h2>
             <p>{movie.genres.map(genreList => <span key={genreList.id}>{genreList.name} | </span>)} Run Time: {runtimeHours}h {runtimeMinutes}m</p>
@@ -41,19 +40,24 @@ function SingleMovie( { movie }) {
             <span>{ratingPercent}</span>
             <h3>Overview</h3>
             <p>{movie.overview}</p>
-        {/* put this in */}
-            <p>Director Producer</p>
+        {/* First 3 Production Crew Members */}
+            <div className="single-movie-production-crew">
+                {movie.credits.crew.slice(0, 3).map(member => 
+                    <div key={member.id} className="single-movie-crew-member">
+                        <p>{member.job}</p>
+                        <p>{member.name}</p>
+                    </div>
+                )}
+            </div>
         </article>
         <article>
-        {/* put this in */}
-            <div className="single-movie-media"></div>
-        </article>
-        <article>
-        {/* put this in */}
+        {/* First 6 actors */}
             <div className="single-movie-cast">
-            {movie.credits.cast.map(actor => 
+            {movie.credits.cast.slice(0, 6).map(actor => 
                 <div key={actor.id} className="single-movie-actor">
-                    {/* insert the photo here */}
+                    <img 
+                        src={`https://image.tmdb.org/t/p/original/${actor.profile_path}`} 
+                        alt={movie.title}/>
                     <p>{actor.name}</p>
                     <p>{actor.character}</p>
                 </div>
@@ -62,8 +66,22 @@ function SingleMovie( { movie }) {
         </article>
         <article>
         {/* put this in */}
+            <div className="single-movie-media">
+                {movie.images.posters.slice(0, 6).map(poster =>
+                    <img 
+                    src={`https://image.tmdb.org/t/p/original/${poster.file_path}`} 
+                    alt={poster.title}/>
+                )}
+            </div>
+        </article>
+        <article> 
+        {/* put this in */}
             <div className="single-movie-trailer">
+                {/* use find to get where type = trailer and youtube */}
+            {/* <iframe width="1280" height="720" src={`https://www.youtube.com/embed/${movie.videos.results[0].key}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
 
+            </iframe> */}
+                
             </div>
         </article>
       </div>
